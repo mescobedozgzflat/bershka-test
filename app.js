17f741364de867c9f6b65e0c6941aee11ee1be3d
Miguel Escobedo <miguel.escobedo@flat101.es>
2018-04-03 16:42:06 +0200

    OK con Herokee

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+/*const fs = require('fs');
 const path = require('path');
 const puppeteer = require('puppeteer');
 async function run() {
@@ -22,4 +22,55 @@ async function run() {
         path: './js/js.js'
     });
 }
-run();
+run();*/
+const express = require('express');
+const app = express();
+const puppeteer = require('puppeteer');
+const port = process.env.PORT || 8081;
+const validUrl = require('valid-url');
+var parseUrl = function(url) {
+    url = decodeURIComponent(url)
+    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+        url = 'http://' + url;
+    }
+    return url;
+};
+app.get('/', function(req, res) {
+    var urlToScreenshot = parseUrl(req.query.url);
+    if (validUrl.isWebUri(urlToScreenshot)) {
+        console.log('Screenshotting: ' + urlToScreenshot);
+        (async () => {
+            const browser = await puppeteer.launch({
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+                headless: false,
+            });
+            urlToScreenshot = 'https://www.bershka.com/es/mujer-c1010193132.html';
+            const page = await browser.newPage();
+            await page.goto(urlToScreenshot , {
+                waitUntil: 'domcontentloaded'
+            });
+            await page.setViewport({
+                width: 1920,
+                height: 1000
+            })
+            page.addStyleTag({
+                path: './css/css.css'
+            });
+            const inputElement = await page.$('.header__searchMobile');
+            page.addScriptTag({
+                path: './js/js.js'
+            });
+            await page.screenshot().then(function(buffer) {
+                res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
+                res.setHeader('Content-Type', 'image/png');
+                res.send(buffer)
+            });
+            await browser.close();
+        })();
+    } else {
+        res.send('Invalid url: ' + urlToScreenshot);
+    }
+});
+app.listen(port, function() {
+    console.log('App listening on port ' + port)
+})
