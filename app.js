2c905e5224aa7b5ba7d40a0de21d9502edfdf4e6
Miguel Escobedo <miguel.escobedo@flat101.es>
2018-04-03 18:02:37 +0200

    aaa

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.get('/', function(req, res) {
         (async () => {
             const browser = await puppeteer.launch({
                 headless: false,
-                args: ['--no-sandbox', '--disable-setuid-sandbox']
+                args: ['--no-sandbox', '--disable-setuid-sandbox','--disable-dev-shm-usage']
             });
             const page = await browser.newPage();
             await page.setViewport({
