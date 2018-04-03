4496b28fb62f8aadf4b0fb91746dbfea388d7abf
Miguel Escobedo <miguel.escobedo@flat101.es>
2018-04-03 17:48:51 +0200

    s

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.get('/', function(req, res) {
         console.log('Screenshotting: ' + urlToScreenshot);
         (async () => {
             const browser = await puppeteer.launch({
-                headless: false,
+                //headless: false,
                 args: ['--no-sandbox', '--disable-setuid-sandbox']
             });
             const page = await browser.newPage();
